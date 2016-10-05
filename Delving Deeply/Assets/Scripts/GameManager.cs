07c3f0ab1d0bed6750ef76013339544efa46f8da
tlhamilt <tlhamilt@mtu.edu>
2016-10-04 22:48:09 -0400

    Attempting to commit

---
+++ Delving Deeply/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+	// Use this for initialization
+	void Awake () {
+	    if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
